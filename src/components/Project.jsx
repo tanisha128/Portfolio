f8c17b4362d5c178de359fcd669bfc33c3fe5092
tanisha128 <tanishateli18@gmail.com>
2025-07-12 22:12:12 +0530

    changed css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Project.css';
+import './project.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
